239b61b62d414829edf7f2d13b3f4f5e54176d43
jasperdevs <jasprcodes@gmail.com>
2026-04-19 22:15:18 -0400

    add brands category with 12 new icons, wire into contact chips

--- src/components/request-form.tsx
+++ src/components/request-form.tsx
@@ -16,22 +16,6 @@ i'd love to see an icon for:
 
 thanks!`;
 
-function XIcon({ size = 14 }: { size?: number }) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="currentColor" aria-hidden>
-      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-    </svg>
-  );
-}
-
-function GithubIcon({ size = 14 }: { size?: number }) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="currentColor" aria-hidden>
-      <path d="M12 .5C5.37.5 0 5.87 0 12.5c0 5.3 3.44 9.8 8.21 11.39.6.11.82-.26.82-.58v-2c-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.34-1.76-1.34-1.76-1.09-.74.08-.73.08-.73 1.2.08 1.84 1.24 1.84 1.24 1.07 1.83 2.8 1.3 3.49.99.11-.78.42-1.3.76-1.6-2.67-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.53.12-3.19 0 0 1-.32 3.3 1.23a11.4 11.4 0 0 1 6 0c2.29-1.55 3.3-1.23 3.3-1.23.66 1.66.24 2.89.12 3.19.77.84 1.24 1.91 1.24 3.22 0 4.61-2.8 5.62-5.48 5.92.43.37.82 1.1.82 2.22v3.29c0 .32.22.7.83.58A12.01 12.01 0 0 0 24 12.5C24 5.87 18.63.5 12 .5" />
-    </svg>
-  );
-}
-
 function CopyBadgeIcon({ size = 12 }: { size?: number }) {
   return (
     <svg
@@ -52,26 +36,18 @@ function CopyBadgeIcon({ size = 12 }: { size?: number }) {
   );
 }
 
-function DiscordIcon({ size = 14 }: { size?: number }) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="currentColor" aria-hidden>
-      <path d="M19.27 5.33A19 19 0 0 0 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09a.1.1 0 0 0-.07-.03c-1.5.26-2.93.71-4.27 1.33a.06.06 0 0 0-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95 0 .02.01.04.03.05a19.3 19.3 0 0 0 5.24 2.65c.03.01.06 0 .07-.02.4-.55.76-1.13 1.07-1.74a.07.07 0 0 0-.04-.09c-.57-.22-1.11-.48-1.64-.78a.07.07 0 0 1-.01-.11q.165-.12.33-.25a.07.07 0 0 1 .07-.01c3.44 1.57 7.15 1.57 10.55 0a.07.07 0 0 1 .07.01q.165.135.33.26a.07.07 0 0 1-.01.11c-.52.31-1.07.56-1.64.78a.07.07 0 0 0-.04.09c.32.61.68 1.19 1.07 1.74.03.01.06.02.09.01a19.2 19.2 0 0 0 5.25-2.65.07.07 0 0 0 .03-.05c.44-4.53-.73-8.46-3.1-11.95a.05.05 0 0 0-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12 0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12 0 1.17-.83 2.12-1.89 2.12" />
-    </svg>
-  );
-}
-
 interface ContactRow {
   id: string;
   label: string;
-  icon: React.ReactNode;
+  iconName: string;
   href?: string;
   copyValue?: string;
 }
 
 const CONTACTS: ContactRow[] = [
-  { id: "x", label: "@jasperdevs", icon: <XIcon />, href: "https://x.com/jasperdevs" },
-  { id: "github", label: "jasperdevs", icon: <GithubIcon />, href: "https://github.com/jasperdevs" },
-  { id: "discord", label: "jasperdevs", icon: <DiscordIcon />, copyValue: "jasperdevs" },
+  { id: "x", label: "@jasperdevs", iconName: "x-twitter", href: "https://x.com/jasperdevs" },
+  { id: "github", label: "jasperdevs", iconName: "github", href: "https://github.com/jasperdevs" },
+  { id: "discord", label: "jasperdevs", iconName: "discord", copyValue: "jasperdevs" },
 ];
 
 interface RequestFormProps {
@@ -161,7 +137,7 @@ export function RequestForm({
           const content = (
             <>
               <span className="inline-flex shrink-0">
-                {copied ? <OddIcon name="check" size={16} /> : c.icon}
+                {copied ? <OddIcon name="check" size={18} /> : <OddIcon name={c.iconName} size={20} />}
               </span>
               <span className="truncate">
                 {copied ? "copied" : c.label}
